13bbb7ad5fb97bd6968dbd080752ff65846811c8
Saira Sofia De Mesa <148078310+sairarat@users.noreply.github.com>
2025-04-01 12:59:00 +0800

    Update TWOBSCS2_GROUP8.java

--- TWOBSCS2_GROUP8.java
+++ TWOBSCS2_GROUP8.java
@@ -3,7 +3,7 @@
 public class TWOBSCS2_GROUP8 {
     public static void main(String[] args) {
         System.out.println("Midterm Project - Divide and Conquer\n\n" +
-                "Link to our GDRIVE FOLDER: https://l.messenger.com/l.php?u=https%3A%2F%2Fdrive.google.com%2Fdrive%2Ffolders%2F1V6hLiyUasScH4T8uUx0fd9b3ifwWz98I%3Fusp%3Dsharing&h=AT0HUNjn6h8qAAQV2J8ymUSSGL71e9H5wuSYkuuUYaPt25PcuPXpAkiubrwNk8HzGV9YTeqtNEfFgcEBfEHcXgJYEr25nqHnSTFvi_YQt0Lom8gDelNajJCfHqe8pAUxO6TH8w");
+                "Link to our GDRIVE FOLDER: https://drive.google.com/drive/folders/1V6hLiyUasScH4T8uUx0fd9b3ifwWz98I");
         System.out.println("Link to our Group Github: https://github.com/sairarat/Josephus_Mingle");
         System.out.println("\nHEXPRESS TEAM:\n" +
                 "ABAD, BAI SAKINA B.\n" +
